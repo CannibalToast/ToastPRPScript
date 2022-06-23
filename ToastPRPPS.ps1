b2bf4c26a5c0b3bc5e92041c8ea289fc9c9ed49a
CannibalToast <54596711+CannibalToast@users.noreply.github.com>
2022-06-23 00:33:43 -0400

    Add files via upload

--- ToastPRPPS.ps1
+++ ToastPRPPS.ps1
@@ -1,7 +1,10 @@
 Write-Host @"
-"CannibalToast's Previsbines Automation Script | HUGE thank you to Soul on discord for helping me make sense of powershell!"
+"CannibalToast's Previsbines Automation Script | HUGE thanks to Soul on discord for helping me make sense of powershell!"
 Stay Toasty!
-PLEASE MAKE SURE THAT CK FIXES IS INSTALLED FROM HERE: https://www.nexusmods.com/fallout4/mods/51165
+PLEASE MAKE SURE THE LATEST VERSION OF CK FIXES IS INSTALLED FROM HERE: https://www.nexusmods.com/fallout4/mods/51165
+DOWNLOAD AND DROP ALL OF THE FOLDERS AND FILES INTO THE FALLOUT 4 FOLDER
+WHEN PROMPTED SELECT YES TO INSTALL F4CK_LOADER
+
 This script is running under the assumption that all prerequisite steps such as:
 Running this in Mod Organizer 2 and this script is in the fallout 4 install directory
 Creating ESP
@@ -11,24 +14,23 @@ All "No Previs" Flags have been removed
 Applying Material Swap script
 Applying Version control script
 Have all been completed
-Please reference the full guide with links to all resources here: https://diskmaster.github.io/ModernPrecombines/MANUAL
-----------------------------------------------------------------------------------------------------------------------
+Please reference full guide with links to all resources here: https://diskmaster.github.io/ModernPrecombines/MANUAL
+--------------------------------------------------------------------------------------------------------------------
 "@
+Do {
+	# PROMPT FOR USER INPUT
+    Write-Host Did you read ALL of the text above about the requirements for this script?`(y/n`)
+    Write-Host
+    $ANSWER = Read-Host Answer
+	Write-Host PLEASE READ THE TEXT ABOVE BEFORE CONTINUING WITH THIS SCRIPT
+}
+Until ($ANSWER -eq 'y')
 #Ask for ESP Name
 $ESP = Read-Host "Please type in the EXACT file name WITHOUT the .esp extension"
-Write-Host "ESP Name: $ESP.esp"
 
 #Ask for xedit directory
 $xEdit = Read-Host "Please give the FULL directory of where FO4Edit is installed"
-Write-Host "xEdit Directory: $xEdit"
 
-#add .enb extension to incompatible enb files
-if (Test-Path "d3d11.dll") 
-{ Rename-Item -Path "d3d11.dll" -NewName "d3d11.dll.enb"
-}
-if (Test-Path "d3dcompiler_46e.dll") 
-{ Rename-Item -Path "d3dcompiler_46e.dll" -NewName "d3dcompiler_46e.dll.enb"
-}
 #Set Variables
 $CK = $null
 $TIMER = "creationkit"
@@ -49,29 +51,23 @@ if (Test-Path "f4ck_loader.exe") {
 }
 
 Write-Host "Using $CK for generation"
-#Precombine Generation
+#PRECOMBINE GENERATION
 if ($CK) {
     Write-Host "Generating Precombines..."
-    Start-Process -FilePath $CK -ArgumentList "-GeneratePrecombined:`"$ESP.esp`" clean all"
-    Start-Sleep -Seconds 5
-    Get-Process | Where-Object {$_.ProcessName -like "$TIMER*"} | Wait-Process
+    Start-Process -FilePath $CK -ArgumentList "-GeneratePrecombined:`"$ESP.esp`" clean all" -wait
     Write-Host "Done!`n"
-    Write-Host "Press OK in xedit and apply this script to $ESP.esp: 03_MergeCombinedObjects.pas"
-    Start-Process -FilePath "$xEdit\fo4edit.exe" -ArgumentList "-quickedit:combinedobjects.esp"
-    Get-Process | Where-Object {$_.ProcessName -like "fo4edit"} | Wait-Process
-	
-#Making Temp BA2
-    Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\Meshes`" -c=`".\Data\$ESP - Main.ba2`""
-    Get-Process | Where-Object {$_.ProcessName -like "archive2.exe"} | Wait-Process
+    Write-Host "Hold shift and press OK in xedit and apply this script to $ESP.esp: 03_MergeCombinedObjects.pas 'n"
+    Start-Process -FilePath "$xEdit\fo4edit.exe" -ArgumentList "-quickedit:combinedobjects.esp" -wait
+
+#MAKING TEMP BA2
+    Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\Meshes`" -c=`".\Data\$ESP - Main.ba2`"" -wait
     Rename-Item -Path ".\data\meshes" -NewName "meshes2"
     Remove-Item -Path ".\Data\CombinedObjects.esp"
-	
+
 #Deleting .PSG file
-    while (!(Test-Path -Path ".\Data\$ESP - Geometry.csg")) {
+		while (!(Test-Path -Path ".\Data\$ESP - Geometry.csg")) {
         Write-Host "Compressing PSG..."
-        Start-Process -FilePath "creationkit.exe" -ArgumentList "-CompressPSG:`"$ESP.esp`""
-        Start-Sleep -Seconds 5
-        Get-Process | Where-Object {$_.ProcessName -like "$TIMER*"} | Wait-Process
+        Start-Process -FilePath "creationkit.exe" -ArgumentList "-CompressPSG:`"$ESP.esp`"" -wait
         Write-Host "Done!`n"
 
         if (Test-Path ".\Data\$ESP - Geometry.csg") {
@@ -83,47 +79,30 @@ if ($CK) {
     }
 	
 #CDX Generation
-    Write-Host "Generating CDX..."
-    Start-Process -FilePath $CK -ArgumentList "-buildcdx:`"$ESP.esp`" clean all"
-    Start-Sleep -Seconds 5
-    Get-Process | Where-Object {$_.ProcessName -like "$TIMER*"} | Wait-Process
+Write-Host "Generating CDX..."
+    Start-Process -FilePath $CK -ArgumentList "-buildcdx:`"$ESP.esp`" clean all" -wait
     Write-Host "Done!`n"
-	
-#Preivs Generation
-    Write-Host "Generating PreVis..."
-    Start-Process -FilePath $CK -ArgumentList "-GeneratePreVisdata:`"$ESP.esp`" clean all"
-    Start-Sleep -Seconds 5
-    Get-Process | Where-Object {$_.ProcessName -like "$TIMER*"} | Wait-Process
-    Write-Host "Done!`n"
-    Write-Host "Press OK in xedit and apply this script to $ESP.esp: MergePrevis"
-    Start-Process -FilePath "$xEdit\fo4edit.exe" -ArgumentList "-quickedit:previs.esp"
-    Get-Process | Where-Object {$_.ProcessName -like "fo4edit"} | Wait-Process
-	
-#Removing .enb Extension from ENB Files
 
-if (Test-Path "d3d11.dll.enb") 
-{ Rename-Item -Path "d3d11.dll.enb" -NewName "d3d11.dll"
-}
-if (Test-Path "d3dcompiler_46e.dll.enb") 
-{ Rename-Item -Path "d3dcompiler_46e.dll.enb" -NewName "d3dcompiler_46e.dll"
-}
-	#Making final BA2
-    Write-Host "Creating .BA2 Archive from files..."
-    Rename-Item -Path ".\data\meshes2" -NewName "meshes"
-     Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\vis,.\data\meshes`" -c=`".\Data\$ESP - Main.ba2`""
-     Start-Sleep -Seconds 5
-     Get-Process | Where-Object {$_.ProcessName -like "archive2.exe"} | Wait-Process
-     #Optional second uncompressed BA2 option. Just remove the #'s below 
-	Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\vis,.\data\meshes`" -compression=None -c=`".\Data\$ESP - Mainc.ba2`""
-	Start-Sleep -Seconds 5
-	Get-Process | Where-Object {$_.ProcessName -like "archive2.exe"} | Wait-Process
-	
-    Remove-Item -Path ".\data\vis" -Recurse
-    Remove-Item -Path ".\data\meshes" -Recurse
-	Remove-Item -Path ".\Data\PreVis.esp"
+#Preivs Generation
+	Write-Host "Generating PreVis..."
+    Start-Process -FilePath $CK -ArgumentList "-GeneratePreVisdata:`"$ESP.esp`" clean all" -wait
     Write-Host "Done!`n"
+    Write-Host "Hold Shift and press OK in xedit and apply this script to $ESP.esp: 05_MergePrevis.pas"
+    Start-Process -FilePath "$xEdit\fo4edit.exe" -ArgumentList "-quickedit:previs.esp" -wait
+
+
+#Making final BA2
+Write-Host "Creating .BA2 Archive from files..."
+    Rename-Item -Path ".\data\meshes2" -NewName "meshes"
+    Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\vis,.\data\meshes`" -c=`".\Data\$ESP - Main.ba2`"" -wait
+     #OPTIONAL SECOND UNCOMPRESSED BA2 OPTION. JUST REMOVE THE #'S BELOW 
+	 #Start-Process -FilePath ".\tools\archive2\archive2" -ArgumentList "`".\Data\vis,.\data\meshes`" -compression=None -c=`".\Data\$ESP - Mainc.ba2`"" -wait
+	Remove-item ".\data\meshes\",".\data\vis\",".\data\previs.esp" -Recurse
+    #Write-Host "Done!`n"
     Read-Host "Thank you for using my script! You may close it now. Stay Toasty!"
+
 } else {
     Write-Host "Failed to find f4ck_loader.exe or creationkit.exe"
 }
+
 exit
